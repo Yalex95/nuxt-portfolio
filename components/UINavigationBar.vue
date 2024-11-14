407de2b5f0d48d373c526600b5759be3d9430204
yerish14 <yerisagui@gmail.com>
2024-11-13 17:57:28 -0800

    md creation

--- components/UINavigationBar.vue
+++ components/UINavigationBar.vue
@@ -1,30 +1,27 @@
 <script setup lang="ts">
 const route = useRoute();
-console.table(route.query);
+console.log(route.path);
 const links = [
   {
     label: "Home",
     icon: "i-heroicons-home",
     to: "/",
-    active: route.path == "/",
+    // active: true,
   },
   {
     label: "Blog",
     icon: "i-heroicons-book-open",
     to: "/blog",
-    active: route.path == "/blog",
   },
   {
     label: "Projects",
     icon: "i-heroicons-chart-bar",
     to: "/projects",
-    active: route.path == "/projects",
   },
   {
     label: "Uses",
     icon: "i-heroicons-command-line",
     to: "/uses",
-    active: route.path == "/uses",
   },
 ];
 </script>
---
+++ pages/blog/[...slug].vue
@@ -0,0 +1,4 @@
+<template>
+  <p>{{ $route.params.slug }}</p>
+  <main><ContentDoc class="prose dark:prose-invert" /></main>
+</template>
